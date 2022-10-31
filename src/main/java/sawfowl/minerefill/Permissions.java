06b17d518f395797066cbbc9ddf5528bc9cd3035
Иван <StalkerIv@yandex.ru>
2022-10-31 16:18:07 +0300

    Update Permissions.java

--- src/main/java/sawfowl/minerefill/Permissions.java
+++ src/main/java/sawfowl/minerefill/Permissions.java
@@ -11,7 +11,6 @@ public class Permissions {
 	public static final String RELOAD = "minerefill.staff.reload";
 	public static final String LIST = "minerefill.user.list";
 	public static final String INFO = "minerefill.user.info";
-	public static final String TELEPORT = "minerefill.user.teleport";
 
 	public static String teleport(UUID mineUUID) {
 		return "minerefill.user.teleport." + mineUUID;
